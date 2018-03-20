f0bc7169ac29fb0826aa8b5a30d13d288192e797
christianhoward <christian.john.howard@gmail.com>
2018-03-20 00:53:07 -0400

    Added Team Logos, OWL Logo and grid layout

--- src/components/TeamFilter.js
+++ src/components/TeamFilter.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react';
 
+import { teams } from './data/data.js';
+
 class TeamFilter extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            teams: [{
-                name: 'SF Shock'
-            }, {
-                name: 'Offline TV and Friends'
-            }]
+            teams: []
         }
+    }
+    componentDidMount() {
+        this.setState({ teams });
     }
     renderTeams() {
         return this.state.teams.map(team => {
             return (
-                <div onClick={(e) => this.props.filterTeams(e)}>{team.name}</div>
+                <div className="team-logo" key={team.name}>
+                    <img src={window.location.origin + `/img/team-logos/${team.logo}`} alt={team.name} onClick={(e) => this.props.filterTeams(e)} data-name={team.name}/>
+                </div>
             );
         });
     }
     render() {
         return (
-            <div>{this.renderTeams()}</div>
+            <div>
+                <div className="owl-logo">
+                    <img src={window.location.origin + '/img/owl-logo.jpg'} data-name={null} onClick={(e) => this.props.filterTeams(e)} alt="Overwatch Leauge Logo" />
+                </div>                    
+                <div className="team-filter-grid">
+                    {this.renderTeams()}
+                </div>
+            </div>
         );
     }
 }
