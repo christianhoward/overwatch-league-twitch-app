a02dcdada8bc31fc109a5fc586bffffb81ef51fe
christianhoward <christian.john.howard@gmail.com>
2018-03-20 12:16:58 -0400

    Added dropdown filter for mobile view and styled the dropdown.

--- src/components/TeamFilter.js
+++ src/components/TeamFilter.js
@@ -16,17 +16,28 @@ class TeamFilter extends Component {
         return this.state.teams.map(team => {
             return (
                 <div className="team-logo" key={team.name}>
-                    <img src={window.location.origin + `/img/team-logos/${team.logo}`} alt={team.name} onClick={(e) => this.props.filterTeams(e)} data-name={team.name}/>
+                    <img src={window.location.origin + `/img/team-logos/${team.logo}`} alt={team.name} onClick={(e) => this.props.filterTeams(e.target.getAttribute('data-name'))} data-name={team.name}/>
                 </div>
             );
+        });
+    }
+    renderTeamDropdown() {
+        return this.state.teams.map(team => {
+            return <option key={team.name} data-name={team.name}>{team.name}</option>
         });
     }
     render() {
         return (
             <div>
                 <div className="owl-logo">
-                    <img src={window.location.origin + '/img/owl-logo.jpg'} data-name={null} onClick={(e) => this.props.filterTeams(e)} alt="Overwatch Leauge Logo" />
-                </div>                    
+                    <img src={window.location.origin + '/img/owl-logo.jpg'} data-name='' onClick={(e) => this.props.filterTeams(e.target.getAttribute('data-name'))} alt="Overwatch Leauge Logo" />
+                </div>  
+                <div className="team-dropdown">
+                    <select value={this.props.filteredTeam} onChange={(e) => this.props.filterTeams(e.target.options[e.target.selectedIndex].getAttribute('data-name'))}>
+                        <option data-name={''}>All</option>
+                        {this.renderTeamDropdown()}
+                    </select>
+                </div>                  
                 <div className="team-filter-grid">
                     {this.renderTeams()}
                 </div>
