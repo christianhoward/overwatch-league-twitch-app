c4d4e4b61539ea37afef3939ca897482f774f946
christianhoward <christian.john.howard@gmail.com>
2018-03-22 02:53:30 -0400

    Added Houston Outlaws, London Spitfire, LA Gladiators, LA Valiant, NY Excelsior and Philadelphia Fusion data.

--- src/components/data/data.js
+++ src/components/data/data.js
@@ -1,220 +1,627 @@
 exports.streams = [
     {
-        user_login: 'tsm_myth',
-        user_id: '110690086',
-        online: false,
-        thumbnail_url: 'sf-shock.png',
-        team: 'SF Shock'
-    }, {
         name: 'DreamKazper',
         user_login: 'dreamkazper',
         user_id: '68624555',
         online: false,
-        thumbnail_url: 'boston-uprising.png',
+        team_url: 'boston-uprising.png',
         team: 'Boston Uprising'
     }, {
         name: 'Snow',
         user_login: 'snow_iwnl',
         user_id: '125494549',
         online: false,
-        thumbnail_url: 'boston-uprising.png',
+        team_url: 'boston-uprising.png',
         team: 'Boston Uprising'
     }, {
         name: 'Gamsu',
         user_login: 'GamsuOW',
         user_id: '129958872',
         online: false,
-        thumbnail_url: 'boston-uprising.png',
+        team_url: 'boston-uprising.png',
         team: 'Boston Uprising'
     }, {
         name: 'NotE',
         user_login: 'NotEvenBleu',
         user_id: '63034034',
         online: false,
-        thumbnail_url: 'boston-uprising.png',
+        team_url: 'boston-uprising.png',
         team: 'Boston Uprising'
     }, {
         name: 'Neko',
         user_login: 'nek0_0w',
         user_id: '144486954',
         online: false,
-        thumbnail_url: 'boston-uprising.png',
+        team_url: 'boston-uprising.png',
         team: 'Boston Uprising'
     }, {
         name: 'Avast',
         user_login: 'avasto',
         user_id: '32905366',
         online: false,
-        thumbnail_url: 'boston-uprising.png',
+        team_url: 'boston-uprising.png',
         team: 'Boston Uprising'
     }, {
         name: 'STRIKER',
         user_login: 'skawn4263',
         user_id: '136116409',
         online: false,
-        thumbnail_url: 'boston-uprising.png',
+        team_url: 'boston-uprising.png',
         team: 'Boston Uprising'
     }, {
         name: 'Kalios',
         user_login: 'ow_kalios',
         user_id: '132763247',
         online: false,
-        thumbnail_url: 'boston-uprising.png',
+        team_url: 'boston-uprising.png',
         team: 'Boston Uprising'
     }, {
         name: 'Mistakes',
         user_login: 'mistakesow',
         user_id: '132369509',
         online: false,
-        thumbnail_url: 'boston-uprising.png',
+        team_url: 'boston-uprising.png',
         team: 'Boston Uprising'
     }, {
         name: 'Kellex',
         user_login: 'keliex',
         user_id: '76581890',
         online: false,
-        thumbnail_url: 'boston-uprising.png',
+        team_url: 'boston-uprising.png',
         team: 'Boston Uprising'
     }, {
         name: 'HarryHook',
         user_login: 'harryhook',
         user_id: '30257355',
         online: false,
-        thumbnail_url: 'dallas-fuel.png',
+        team_url: 'dallas-fuel.png',
         team: 'Dallas Fuel'
     }, {
         name: 'aKm',
         user_login: 'akm_tv',
         user_id: '31670989',
         online: false,
-        thumbnail_url: 'dallas-fuel.png',
+        team_url: 'dallas-fuel.png',
         team: 'Dallas Fuel'
     }, {
         name: 'Rascal',
         user_login: 'rascal0723',
         user_id: '144636878',
         online: false,
-        thumbnail_url: 'dallas-fuel.png',
+        team_url: 'dallas-fuel.png',
         team: 'Dallas Fuel'
     }, {
         name: 'OGE',
         user_login: 'oge_ow',
         user_id: '191580639',
         online: false,
-        thumbnail_url: 'dallas-fuel.png',
+        team_url: 'dallas-fuel.png',
         team: 'Dallas Fuel'
     }, {
         name: 'Taimou',
         user_login: 'taimoutv',
         user_id: '54581600',
         online: false,
-        thumbnail_url: 'dallas-fuel.png',
+        team_url: 'dallas-fuel.png',
         team: 'Dallas Fuel'
     }, {
         name: 'Custa',
         user_login: 'custa',
         user_id: '114865766',
         online: false,
-        thumbnail_url: 'dallas-fuel.png',
+        team_url: 'dallas-fuel.png',
         team: 'Dallas Fuel'
     }, {
         name: 'chipshajen',
         user_login: 'chipshajen',
         user_id: '31735052',
         online: false,
-        thumbnail_url: 'dallas-fuel.png',
+        team_url: 'dallas-fuel.png',
         team: 'Dallas Fuel'
     }, {
         name: 'Mickie',
         user_login: 'mickiepp',
         user_id: '141713412',
         online: false,
-        thumbnail_url: 'dallas-fuel.png',
+        team_url: 'dallas-fuel.png',
         team: 'Dallas Fuel'
     }, {
         name: 'Seagull',
         user_login: 'A_Seagull',
         user_id: '19070311',
         online: false,
-        thumbnail_url: 'dallas-fuel.png',
+        team_url: 'dallas-fuel.png',
         team: 'Dallas Fuel'
     }, {
         name: 'cocco',
         user_login: 'cocco',
         user_id: '117748199',
         online: false,
-        thumbnail_url: 'dallas-fuel.png',
+        team_url: 'dallas-fuel.png',
         team: 'Dallas Fuel'
     }, {
         name: 'EFFECT',
         user_login: 'eff2ct',
         user_id: '135013913',
         online: false,
-        thumbnail_url: 'dallas-fuel.png',
+        team_url: 'dallas-fuel.png',
         team: 'Dallas Fuel'
     }, {
         name: 'zappis',
         user_login: 'zappis',
         user_id: '29493830',
         online: false,
-        thumbnail_url: 'florida-mayhem.png',
+        team_url: 'florida-mayhem.png',
         team: 'Florida Mayhem'
     }, {
         name: 'Manneten',
         user_login: 'manneten',
         user_id: '46769380',
         online: false,
-        thumbnail_url: 'florida-mayhem.png',
+        team_url: 'florida-mayhem.png',
         team: 'Florida Mayhem'
     }, {
         name: 'Sayaplayer',
         user_login: 'hjm3935',
         user_id: '85352894',
         online: false,
-        thumbnail_url: 'florida-mayhem.png',
+        team_url: 'florida-mayhem.png',
         team: 'Florida Mayhem'
     }, {
         name: 'Logix',
         user_login: 'logixow',
         user_id: '26508156',
         online: false,
-        thumbnail_url: 'florida-mayhem.png',
+        team_url: 'florida-mayhem.png',
         team: 'Florida Mayhem'
     }, {
         name: 'Zebbosai',
         user_login: 'zebbosai',
         user_id: '23064268',
         online: false,
-        thumbnail_url: 'florida-mayhem.png',
+        team_url: 'florida-mayhem.png',
         team: 'Florida Mayhem'
     }, {
         name: 'CWoosH',
         user_login: 'cwoosh',
         user_id: '123618783',
         online: false,
-        thumbnail_url: 'florida-mayhem.png',
+        team_url: 'florida-mayhem.png',
         team: 'Florida Mayhem'
     }, {
         name: 'TviQ',
         user_login: 'tvique',
         user_id: '27728003',
         online: false,
-        thumbnail_url: 'florida-mayhem.png',
+        team_url: 'florida-mayhem.png',
         team: 'Florida Mayhem'
     }, {
         name: 'aWesomeGuy',
         user_login: 'aWesomeGuy_ow',
         user_id: '127597366',
         online: false,
-        thumbnail_url: 'florida-mayhem.png',
+        team_url: 'florida-mayhem.png',
         team: 'Florida Mayhem'
     }, {
         name: 'Zuppeh',
         user_login: 'zuppehow',
         user_id: '103006437',
         online: false,
-        thumbnail_url: 'florida-mayhem.png',
+        team_url: 'florida-mayhem.png',
         team: 'Florida Mayhem'
+    }, {
+        name: 'Muma',
+        user_login: 'muma',
+        user_id: '96447492',
+        online: false,
+        team_url: 'houston-outlaws.png',
+        team: 'Houston Outlaws'
+    }, {
+        name: 'Bani',
+        user_login: 'BaniOW',
+        user_id: '22468499',
+        online: false,
+        team_url: 'houston-outlaws.png',
+        team: 'Houston Outlaws'
+    }, {
+        name: 'Clockwork',
+        user_login: 'clockwork',
+        user_id: '24644343',
+        online: false,
+        team_url: 'houston-outlaws.png',
+        team: 'Houston Outlaws'
+    }, {
+        name: 'Mendokusaii',
+        user_login: 'mendokusaii',
+        user_id: '57717183',
+        online: false,
+        team_url: 'houston-outlaws.png',
+        team: 'Houston Outlaws'
+    }, {
+        name: 'Boink',
+        user_login: 'forceboink',
+        user_id: '22883056',
+        online: false,
+        team_url: 'houston-outlaws.png',
+        team: 'Houston Outlaws'
+    }, {
+        name: 'FCTFCTN',
+        user_login: 'factfiction',
+        user_id: '107667412',
+        online: false,
+        team_url: 'houston-outlaws.png',
+        team: 'Houston Outlaws'
+    }, {
+        name: 'LiNKzr',
+        user_login: 'linkzr',
+        user_id: '68558124',
+        online: false,
+        team_url: 'houston-outlaws.png',
+        team: 'Houston Outlaws'
+    }, {
+        name: 'SPREE',
+        user_login: 'spree',
+        user_id: '37351711',
+        online: false,
+        team_url: 'houston-outlaws.png',
+        team: 'Houston Outlaws'
+    }, {
+        name: 'Rawkus',
+        user_login: 'itsrawkus',
+        user_id: '27855530',
+        online: false,
+        team_url: 'houston-outlaws.png',
+        team: 'Houston Outlaws'
+    }, {
+        name: 'Jake',
+        user_login: 'jake_ow',
+        user_id: '135360961',
+        online: false,
+        team_url: 'houston-outlaws.png',
+        team: 'Houston Outlaws'
+    }, {
+        name: 'Coolmatt',
+        user_login: 'coolmatt69',
+        user_id: '36043286',
+        online: false,
+        team_url: 'houston-outlaws.png',
+        team: 'Houston Outlaws'
+    }, {
+        name: 'Gesture',
+        user_login: 'GestureOW',
+        user_id: '170974282',
+        online: false,
+        team_url: 'london-spitfire.png',
+        team: 'London Spitfire'
+    }, {
+        name: 'birdring',
+        user_login: 'birdring_ow',
+        user_id: '144548334',
+        online: false,
+        team_url: 'london-spitfire.png',
+        team: 'London Spitfire'
+    }, {
+        name: 'Bdosin',
+        user_login: 'bdosin_ow',
+        user_id: '163860353',
+        online: false,
+        team_url: 'london-spitfire.png',
+        team: 'London Spitfire'
+    }, {
+        name: 'NUS',
+        user_login: 'asdzxcl2',
+        user_id: '129550511',
+        online: false,
+        team_url: 'london-spitfire.png',
+        team: 'London Spitfire'
+    }, {
+        name: 'Hooreg',
+        user_login: 'hooregow',
+        user_id: '152172663',
+        online: false,
+        team_url: 'london-spitfire.png',
+        team: 'London Spitfire'
+    }, {
+        name: 'Fury',
+        user_login: 'furyy_d',
+        user_id: '131542111',
+        online: false,
+        team_url: 'london-spitfire.png',
+        team: 'London Spitfire'
+    }, {
+        name: 'HaGoPeun',
+        user_login: 'hagopeun',
+        user_id: '130599198',
+        online: false,
+        team_url: 'london-spitfire.png',
+        team: 'London Spitfire'
+    }, {
+        name: 'WooHyaL',
+        user_login: 'woohyal_ow',
+        user_id: '140804873',
+        online: false,
+        team_url: 'london-spitfire.png',
+        team: 'London Spitfire'
+    }, {
+        name: 'Profit',
+        user_login: 'profitow',
+        user_id: '154784259',
+        online: false,
+        team_url: 'london-spitfire.png',
+        team: 'London Spitfire'
+    }, {
+        name: 'TiZi',
+        user_login: 'wkdgus884',
+        user_id: '137418482',
+        online: false,
+        team_url: 'london-spitfire.png',
+        team: 'London Spitfire'
+    }, {
+        name: 'Closer',
+        user_login: 'closerow',
+        user_id: '138973451',
+        online: false,
+        team_url: 'london-spitfire.png',
+        team: 'London Spitfire'
+    }, {
+        name: 'Asher',
+        user_login: 'asher_ow',
+        user_id: '153223108',
+        online: false,
+        team_url: 'la-gladiators.png',
+        team: 'Los Angeles Gladiators'
+    }, {
+        name: 'Surefour',
+        user_login: 'surefour',
+        user_id: '2982838',
+        online: false,
+        team_url: 'la-gladiators.png',
+        team: 'Los Angeles Gladiators'
+    }, {
+        name: 'iRemiix',
+        user_login: 'iremiixz',
+        user_id: '48730158',
+        online: false,
+        team_url: 'la-gladiators.png',
+        team: 'Los Angeles Gladiators'
+    }, {
+        name: 'Fissure',
+        user_login: 'fissure_ow',
+        user_id: '144636778',
+        online: false,
+        team_url: 'la-gladiators.png',
+        team: 'Los Angeles Gladiators'
+    }, {
+        name: 'Bischu',
+        user_login: 'bischulol',
+        user_id: '35671232',
+        online: false,
+        team_url: 'la-gladiators.png',
+        team: 'Los Angeles Gladiators'
+    }, {
+        name: 'Shaz',
+        user_login: 'shaz_ow',
+        user_id: '31047413',
+        online: false,
+        team_url: 'la-gladiators.png',
+        team: 'Los Angeles Gladiators'
+    }, {
+        name: 'Hydration',
+        user_login: 'hydration',
+        user_id: '53176196',
+        online: false,
+        team_url: 'la-gladiators.png',
+        team: 'Los Angeles Gladiators'
+    }, {
+        name: 'BigGoose',
+        user_login: 'biggooseow',
+        user_id: '41703896',
+        online: false,
+        team_url: 'la-gladiators.png',
+        team: 'Los Angeles Gladiators'
+    }, {
+        name: 'Silkthread',
+        user_login: 'silkthread',
+        user_id: '26437807',
+        online: false,
+        team_url: 'la-valiant.png',
+        team: 'Los Angeles Valiant'
+    }, {
+        name: 'Grimreality',
+        user_login: 'grimrealityow',
+        user_id: '136539228',
+        online: false,
+        team_url: 'la-valiant.png',
+        team: 'Los Angeles Valiant'
+    }, {
+        name: 'Kariv',
+        user_login: 'kariv2',
+        user_id: '135122504',
+        online: false,
+        team_url: 'la-valiant.png',
+        team: 'Los Angeles Valiant'
+    }, {
+        name: 'Fate',
+        user_login: 'fate_ow',
+        user_id: '138037386',
+        online: false,
+        team_url: 'la-valiant.png',
+        team: 'Los Angeles Valiant'
+    }, {
+        name: 'Verbo',
+        user_login: 'verboow',
+        user_id: '141190296',
+        online: false,
+        team_url: 'la-valiant.png',
+        team: 'Los Angeles Valiant'
+    }, {
+        name: 'Envy',
+        user_login: 'envy0w',
+        user_id: '160050719',
+        online: false,
+        team_url: 'la-valiant.png',
+        team: 'Los Angeles Valiant'
+    }, {
+        name: 'Space',
+        user_login: 'space_ow',
+        user_id: '147953635',
+        online: false,
+        team_url: 'la-valiant.png',
+        team: 'Los Angeles Valiant'
+    }, {
+        name: 'Numlocked',
+        user_login: 'numlocked',
+        user_id: '8725477',
+        online: false,
+        team_url: 'la-valiant.png',
+        team: 'Los Angeles Valiant'
+    }, {
+        name: 'Unkoe',
+        user_login: 'unkoeee',
+        user_id: '50447542',
+        online: false,
+        team_url: 'la-valiant.png',
+        team: 'Los Angeles Valiant'
+    }, {
+        name: 'Agilities',
+        user_login: 'agilities',
+        user_id: '126162810',
+        online: false,
+        team_url: 'la-valiant.png',
+        team: 'Los Angeles Valiant'
+    }, {
+        name: 'Soon',
+        user_login: 'soonsm',
+        user_id: '72915687',
+        online: false,
+        team_url: 'la-valiant.png',
+        team: 'Los Angeles Valiant'
+    }, {
+        name: 'Saebyeolbe',
+        user_login: 'saebyeolbe',
+        user_id: '140197662',
+        online: false,
+        team_url: 'ny-excelsior.png',
+        team: 'New York Excelsior'
+    }, {
+        name: 'Meko',
+        user_login: 'mekoppa',
+        user_id: '152073095',
+        online: false,
+        team_url: 'ny-excelsior.png',
+        team: 'New York Excelsior'
+    }, {
+        name: 'Pine',
+        user_login: 'tf2pine',
+        user_id: '62597620',
+        online: false,
+        team_url: 'ny-excelsior.png',
+        team: 'New York Excelsior'
+    }, {
+        name: 'Janus',
+        user_login: 'januspy',
+        user_id: '75603583',
+        online: false,
+        team_url: 'ny-excelsior.png',
+        team: 'New York Excelsior'
+    }, {
+        name: 'Jjonak',
+        user_login: 'jjonaklove',
+        user_id: '140577538',
+        online: false,
+        team_url: 'ny-excelsior.png',
+        team: 'New York Excelsior'
+    }, {
+        name: 'Mano',
+        user_login: 'manoowl',
+        user_id: '169109715',
+        online: false,
+        team_url: 'ny-excelsior.png',
+        team: 'New York Excelsior'
+    }, {
+        name: 'Libero',
+        user_login: 'liberot99',
+        user_id: '126936223',
+        online: false,
+        team_url: 'ny-excelsior.png',
+        team: 'New York Excelsior'
+    }, {
+        name: 'Ark',
+        user_login: 'arkyjun',
+        user_id: '138868831',
+        online: false,
+        team_url: 'ny-excelsior.png',
+        team: 'New York Excelsior'
+    }, {
+        name: 'Joemeister',
+        user_login: 'JoemeisterOW',
+        user_id: '128109321',
+        online: false,
+        team_url: 'philadelphia-fusion.png',
+        team: 'Philadelphia Fusion'
+    }, {
+        name: 'Boombox',
+        user_login: 'boomboxOW',
+        user_id: '87917078',
+        online: false,
+        team_url: 'philadelphia-fusion.png',
+        team: 'Philadelphia Fusion'
+    }, {
+        name: 'Carpe',
+        user_login: 'carpe',
+        user_id: '148019820',
+        online: false,
+        team_url: 'philadelphia-fusion.png',
+        team: 'Philadelphia Fusion'
+    }, {
+        name: 'fragi',
+        user_login: 'fragiow',
+        user_id: '37210499',
+        online: false,
+        team_url: 'philadelphia-fusion.png',
+        team: 'Philadelphia Fusion'
+    }, {
+        name: 'Eqo',
+        user_login: 'eqobot',
+        user_id: '144332450',
+        online: false,
+        team_url: 'philadelphia-fusion.png',
+        team: 'Philadelphia Fusion'
+    }, {
+        name: 'ShaDowBurn',
+        user_login: 'SDBurn',
+        user_id: '45165401',
+        online: false,
+        team_url: 'philadelphia-fusion.png',
+        team: 'Philadelphia Fusion'
+    }, {
+        name: 'Neptuno',
+        user_login: 'neptunoow',
+        user_id: '44003728',
+        online: false,
+        team_url: 'philadelphia-fusion.png',
+        team: 'Philadelphia Fusion'
+    }, {
+        name: 'DayFly',
+        user_login: 'dayfly',
+        user_id: '126170972',
+        online: false,
+        team_url: 'philadelphia-fusion.png',
+        team: 'Philadelphia Fusion'
+    }, {
+        name: 'Hotba',
+        user_login: 'hotba_ow',
+        user_id: '138328501',
+        online: false,
+        team_url: 'philadelphia-fusion.png',
+        team: 'Philadelphia Fusion'
+    }, {
+        name: 'Poko',
+        user_login: 'poko',
+        user_id: '53265819',
+        online: false,
+        team_url: 'philadelphia-fusion.png',
+        team: 'Philadelphia Fusion'
     }
 ];
 
