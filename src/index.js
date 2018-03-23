32bf5f8133662503c52a9b26d78345324b57e62c
christianhoward <christian.john.howard@gmail.com>
2018-03-23 11:17:16 -0400

    Implementing react-ga for Google Analytics.

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
+import ReactGA from 'react-ga';
+React.GA.initialize(process.env.REACT_APP_GA);
+ReactGA.pageview(window.location.pathname);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
