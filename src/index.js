f8e1086ad10d5e5171058b832728a371eeeaa4f7
christianhoward <christian.john.howard@gmail.com>
2018-03-23 11:22:30 -0400

    Typo in ReactGA.initialize()

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 import ReactGA from 'react-ga';
-React.GA.initialize(process.env.REACT_APP_GA);
+ReactGA.initialize(process.env.REACT_APP_GA);
 ReactGA.pageview(window.location.pathname);
 
 ReactDOM.render(<App />, document.getElementById('root'));
