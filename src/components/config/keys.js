4b0ec6d3f436794b7aa80f500085d0326b259dda
christianhoward <christian.john.howard@gmail.com>
2018-03-23 10:23:04 -0400

    Testing CLIENTID on Heroku.

--- src/components/config/keys.js
+++ src/components/config/keys.js
@@ -2,5 +2,5 @@ if (process.env.NODE_ENV === 'development') {
     module.exports = require('./dev');
 } else {
     module.exports = require('./prod');
-    console.log('prod');
+    console.log('prod', process.node.CLIENTID);
 }
